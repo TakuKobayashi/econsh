8082a590391fccfe1c5b552242ed6fa84af82f62
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-02-28 10:52:10 +0900

    setup serverless framework

---
+++ serverless/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  printWidth: 140,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  parser: 'typescript',
+};
---
+++ serverless/src/app.ts
@@ -0,0 +1,12 @@
+import serverlessExpress from '@vendia/serverless-express';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.get('/test', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+export const handler = serverlessExpress({ app });
